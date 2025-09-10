d5834629ef141ecaa7cbbb110a73291cfa3cfaed
kancheti jyoshnavi <kanchetijyoshna@gmail.com>
2025-09-10 11:14:20 +0530

    Add files via upload

---
+++ Pattren_08.java
@@ -0,0 +1,39 @@
+package com.codegnan.pattrens;
+
+import java.util.Scanner;
+
+public class Pattren_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int s = 0; s < n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1) || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int s = 0; s < n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1) || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
